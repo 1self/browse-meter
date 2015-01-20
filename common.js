8a208bfcd11c130048c88c34a1ab9e6f3bbf3a64
Devaroop <devaroop123@yahoo.co.in>
2015-01-20 13:10:33 +0530

    add new websites

--- common.js
+++ common.js
@@ -1,6 +1,26 @@
 var PREDEFINED_HOSTS = [
     "www.youtube.com",
-    "www.facebook.com"
+    "www.facebook.com",
+    "en.wikipedia.org",
+    "www.amazon.com",
+    "www.yahoo.com",
+    "www.google.com",
+    "www.baidu.com",
+    "twitter.com",
+    "www.linkedin.com",
+    "www.ebay.com",
+    "www.bing.com",
+    "www.blogger.com",
+    "www.reddit.com",
+    "www.pinterest.com",
+    "instagram.com",
+    "imgur.com",
+    "www.paypal.com",
+    "www.netflix.com",
+    "www.dropbox.com",
+    "edition.cnn.com",
+    "www.walmart.com",
+    "www.dailymotion.com"
 ];
 
 var oneself = new Lib1self({
@@ -9,6 +29,14 @@ var oneself = new Lib1self({
     "appId": "app-id-75441150bce89bef25c26a4e2acf429e",
     "appSecret": "app-secret-f81d333ac301ec2b0dd5d757ffb2db6dc6607966a52a8a5d06b58f84b3a72446"
 });
+
+if(!window.localStorage.data){
+    oneself.registerStream(function(response) {
+        if (response) {
+            window.localStorage.data = JSON.stringify(response);
+        }
+    });
+}
 
 var getVizUrl = function(host) {
     var data = JSON.parse(window.localStorage.data),
@@ -31,6 +59,7 @@ var getVizUrl = function(host) {
 
 
 var constructEventAndSend = function(url){
+
     var parser = document.createElement('a');
     parser.href = url;
     
@@ -56,11 +85,3 @@ var constructEventAndSend = function(url){
     });
 };
 
-
-if(!window.localStorage.data){
-    oneself.registerStream(function(response) {
-        if (response) {
-            window.localStorage.data = JSON.stringify(response);
-        }
-    });
-}
