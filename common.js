c4b7055513260233fb441c7efb88a5ffa35f0436
Devaroop <devaroop123@yahoo.co.in>
2015-01-21 15:51:21 +0530

    determine urls using host instead of wildcard. Thanks Chinmay

--- common.js
+++ common.js
@@ -1,27 +1,4 @@
-var PREDEFINED_HOSTS = [
-    "www.youtube.com",
-    "www.facebook.com",
-    "en.wikipedia.org",
-    "www.amazon.com",
-    "www.yahoo.com",
-    "www.google.com",
-    "www.baidu.com",
-    "twitter.com",
-    "www.linkedin.com",
-    "www.ebay.com",
-    "www.bing.com",
-    "www.blogger.com",
-    "www.reddit.com",
-    "www.pinterest.com",
-    "instagram.com",
-    "imgur.com",
-    "www.paypal.com",
-    "www.netflix.com",
-    "www.dropbox.com",
-    "edition.cnn.com",
-    "www.walmart.com",
-    "www.dailymotion.com"
-];
+var PREDEFINED_HOSTS = ["youtube", "facebook", "google", "wikipedia", "amazon", "yahoo", "twitter", "linkedin", "ebay", "reddit", "pinterest", "instagram", "imgur", "netflix", "msn", "stackoverflow", "dropbox", "cnn", "walmart", "dailymotion", "baidu", "bing", "blogger", "paypal"];
 
 var oneself = new Lib1self({
     "appName": 'Hello, 1self',
@@ -59,15 +36,13 @@ var getVizUrl = function(host) {
 
 
 var constructEventAndSend = function(url){
+    var host = parseURL(url).host;
 
-    var parser = document.createElement('a');
-    parser.href = url;
-    
-    if(PREDEFINED_HOSTS.indexOf(parser.hostname) === -1){
+    if(!isHostInTrackingList(host)){
         return;
     }
 
-    var objectTags = [parser.hostname],
+    var objectTags = [host],
     actionTags = ["browse"],
     properties = {};
 
@@ -85,3 +60,45 @@ var constructEventAndSend = function(url){
     });
 };
 
+var isHostInTrackingList = function(host){
+    return PREDEFINED_HOSTS.indexOf(host) !== -1;
+};
+
+function parseURL(url){
+    parsed_url = {}
+
+    if ( url == null || url.length == 0 )
+        return parsed_url;
+
+    protocol_i = url.indexOf('://');
+    parsed_url.protocol = url.substr(0,protocol_i);
+
+    remaining_url = url.substr(protocol_i + 3, url.length);
+    domain_i = remaining_url.indexOf('/');
+    domain_i = domain_i == -1 ? remaining_url.length - 1 : domain_i;
+    parsed_url.domain = remaining_url.substr(0, domain_i);
+    parsed_url.path = domain_i == -1 || domain_i + 1 == remaining_url.length ? null : remaining_url.substr(domain_i + 1, remaining_url.length);
+
+    domain_parts = parsed_url.domain.split('.');
+    switch ( domain_parts.length ){
+        case 2:
+          parsed_url.subdomain = null;
+          parsed_url.host = domain_parts[0];
+          parsed_url.tld = domain_parts[1];
+          break;
+        case 3:
+          parsed_url.subdomain = domain_parts[0];
+          parsed_url.host = domain_parts[1];
+          parsed_url.tld = domain_parts[2];
+          break;
+        case 4:
+          parsed_url.subdomain = domain_parts[0];
+          parsed_url.host = domain_parts[1];
+          parsed_url.tld = domain_parts[2] + '.' + domain_parts[3];
+          break;
+    }
+
+    parsed_url.parent_domain = parsed_url.host + '.' + parsed_url.tld;
+
+    return parsed_url;
+}
